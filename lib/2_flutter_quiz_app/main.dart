8fc9dc7edcf8d48dbdd5eb87568e25f663f1fdb2
Terris <27739101+Terristwj@users.noreply.github.com>
2023-09-30 00:42:11 +0800

    Section3_QuizApp_Part1

---
+++ lib/2_flutter_quiz_app/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learnings/2_flutter_quiz_app/quiz.dart';
+
+void main() {
+  runApp(const Quiz());
+}
---
+++ lib/2_flutter_quiz_app/quiz.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learnings/2_flutter_quiz_app/question_screen.dart';
+import 'package:flutter_learnings/2_flutter_quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.deepPurple.shade700,
+                Colors.deepPurple.shade400,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
